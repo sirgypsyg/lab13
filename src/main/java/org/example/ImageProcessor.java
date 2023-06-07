c87ba1f285e03f1e1e0f7aa33138844f5db378d3
kubaobara <siemagosciu32@gmail.com>
2023-06-07 14:56:25 +0200

    z2/3

--- src/main/java/org/example/ImageProcessor.java
+++ src/main/java/org/example/ImageProcessor.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,10 +16,53 @@ public class ImageProcessor {
         image = ImageIO.read(file);
         height = image.getHeight();
         width = image.getWidth();
-    }
+        int color = image.getRGB(0, 0);
 
+//        int r = (color >> 16) & 0xff;
+//        int g = (color >> 8) & 0xff;
+//        int b = color & 0xff;
+//
+//        r+=100;
+//        g+=100;
+//        b+=100;
+//
+//        int result = (r << 16) | (g << 8) | b;
+//
+//        image.setRGB(0,0,result);
+
+        Color color1 = new Color(color);
+        color1.getRed();
+
+
+        new Color(color1.getBlue(), color1.getRed(), color1.getBlue());
+    }
     public void saveImage(String path) throws IOException {
         File file = new File(path);
         ImageIO.write(image, "png", new File(path));
     }
+
+    public int getPixelAfterBright(int oldColor, int factor){
+        return 0;
+    }
+
+    public void setBrightness(int factor) {
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                Color color = new Color(image.getRGB(x,y));
+
+                int red = color.getRed() + factor;
+                int green = color.getGreen() + factor;
+                int blue = color.getBlue() + factor;
+
+                red = Math.max(0, Math.min(255, red));
+                green = Math.max(0, Math.min(255, green));
+                blue = Math.max(0, Math.min(255, blue));
+
+                Color newColor = new Color(red, green, blue);
+                image.setRGB(x, y, newColor.getRGB());
+
+            }
+        }
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        long start = System.currentTimeMillis();
+
+        String path = "/Users/kubaobara/Desktop/Screenshot 2023-06-06 at 13.57.30.png";
+        ImageProcessor imageProcessor = new ImageProcessor();
+        imageProcessor.loadImage(path);
+        imageProcessor.setBrightness(100);
+        imageProcessor.saveImage("/Users/kubaobara/Studia/WykladOOP/lab13/src/pliczek.png");
+
+        long end = System.currentTimeMillis();
+        System.out.println(start-end);
+
+        int cores = Runtime.getRuntime().availableProcessors();
+
+        Thread thread1 = new Thread(() ->{
+            /////
+        });
+        Thread thread2 = new Thread(() ->{
+            /////
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        ExecutorService executorService = Executors.newFixedThreadPool()
     }
+
 }
